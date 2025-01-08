1f4974f32b4fe7bf9f50e6c0d6a70a5558dc497d
RangikaShalani <rangika2520@gmail.com>
2025-01-08 22:41:26 +0530

    task2 completed, add Pagination for page sliceing

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -1,20 +1,38 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import UserCard from '../UserCard';
 import '../style/common.css';
 import { StoreContext } from '../../StoreProvider';
+import Pagination from '@mui/material/Pagination';
 
 const UsersList = () => {
     const { users } = useContext(StoreContext);
 
+    const [page, setPage] = useState(1);
+    const usersPerPage = 10;
+
+    const handleChange = (event, value) => {
+        setPage(value);
+    };
+    const startNumber = (page - 1) * usersPerPage;
+    const endNumber = page * usersPerPage;
     return (
         <div className='userList-main-cmp' >
             <div className='userList-card-cmp'>
                 {users.length > 0 ? (
-                    users.map(user => <UserCard key={user.id} user={user} />)
+                    users.slice(startNumber, endNumber).map(user => <UserCard key={user.id} user={user} />)
                 ) : (
                     <p>No users available. Click "Load Users" to fetch data.</p>
                 )}
             </div>
+            {users.length === 0 ? null : (
+                <Pagination
+                    count={Math.ceil(users.length / usersPerPage)}
+                    page={page}
+                    onChange={handleChange}
+                    variant="outlined"
+                    color="primary" />
+            )}
+
         </div>
     );
 }
